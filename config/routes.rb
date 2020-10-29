f0435a812c3d1002f233b1c19e79b8ad99ca0a11
EC2 Default User <ec2-user@ip-172-31-6-175.ec2.internal>
2020-10-29 12:44:13 +0000

    [finish]5-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root  'post_images#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
