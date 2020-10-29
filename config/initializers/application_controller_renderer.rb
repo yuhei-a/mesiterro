2d51c3271d46e43fbe782ead9e347f0479ace16d
EC2 Default User <ec2-user@ip-172-31-6-175.ec2.internal>
2020-10-29 07:31:05 +0000

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '254b5c786398bbf5346f281c7a46f0363ee620d002804b7a2e99677d8f971a04b3e8020d1da05cc3a9cbcba410be34d3746e63e6f7c3022e3faa139bd8c3fd9e'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root  'post_images#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :post_images, only: [:new, :create, :index, :show]
 end
